977790aabe491f9f58cf0d641fa24c8209a64c41
root <root@DESKTOP-STNC3N3.localdomain>
2022-07-05 13:12:24 -0300

    Ready

---
+++ README.md
@@ -0,0 +1,2 @@
+this project was made based on Roger Melo project how to do a Pokedex i made the way he teach's but
+i decided to by my way to understando more how it's work
--- options.js
+++ options.js
@@ -1,6 +1,8 @@
 const buttonClick = document.querySelector("#btnClick");
 const lisContent = document.querySelector(".names");
 const textNumber = document.querySelector("#number");
+const divOptions = document.querySelector(".insert-class");
+const worningMsg = document.querySelector(".worning-msg");
 
 const verify_itens = () => {
   if (lisContent.children.length > 0) {
@@ -14,9 +16,21 @@ const clean_Lis_Content = () => {
   }
 };
 
+const worning_Msg_If_Number_Above70 = () => {
+  worningMsg.style.display = "";
+  setTimeout(() => {
+    worningMsg.style.display = "none";
+  }, 8000);
+};
+
 buttonClick.addEventListener("click", () => {
   if (verify_itens() > 0) {
     clean_Lis_Content();
   }
+  const getValue = Number(textNumber.value);
+  if (getValue > 70) {
+    worning_Msg_If_Number_Above70();
+  }
+
   init_Pokemons(textNumber.value);
 });
